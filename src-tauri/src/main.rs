9bbc66a078fae4185d3b08cfd7f8810cb9c737cf
Saigo1997 <a.saigo1997@gmail.com>
2023-10-31 03:25:43 +0900

    一旦最低限の機能が使えるところまで実装

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,132 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use reqwest::Client;
+use serde::Serialize;
+use serde::Deserialize;
+use std::env;
+use dotenv::dotenv;
+use std::collections::HashMap;
+use std::sync::Mutex;
+use once_cell::sync::Lazy;
+use std::fs;
+use std::fs::File;
+use std::io::{self, Read, Write, BufReader};
+
+static TRANSLATE_CACHE: Lazy<Mutex<HashMap<String, String>>> = Lazy::new(|| {
+    Mutex::new(HashMap::new())
+});
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Translation {
+    detected_source_language: String,
+    text: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TranslationResponse {
+    translations: Vec<Translation>,
+}
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn translate_into_jananese(phrase: &str) -> Result<String, String> {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime.block_on(async_translate_into_jananese(phrase))
+}
+
+async fn async_translate_into_jananese(phrase: &str) -> Result<String, String> {
+    let mut cache = TRANSLATE_CACHE.lock().unwrap();
+
+    // キャッシュに引っかかったらそれを返す
+    if let Some(ja_phrase) = cache.get(phrase).cloned() {
+        println!("cache hit! {} -> {}", phrase, ja_phrase);
+        return Ok(ja_phrase);
+    }
+
+    // キャッシュになかったらDeepLを使って翻訳する
+    let deepl_auth_key = env::var("DEEPL_AUTH_KEY").expect("DEEPL_AUTH_KEY is not defined");
+    let client = Client::new();
+    let url = "https://api-free.deepl.com/v2/translate";
+    let params = [
+        ("text", phrase),
+        ("source_lang", "EN"),
+        ("target_lang", "JA"),
+    ];
+    let result = client
+        .post(url)
+        .header("Authorization", format!("DeepL-Auth-Key {}", deepl_auth_key))
+        .form(&params)
+        .send()
+        .await;
+    match result {
+        Ok(response) => {
+            let res = parse_response(response).await?;
+            println!("{:?}", res);
+            println!("{}", res.translations[0].text);
+            if res.translations.len() > 0 {
+                let ja_phrase = res.translations[0].text.clone();
+                cache.insert(phrase.to_string(), ja_phrase.clone());
+                return Ok(ja_phrase);
+            }
+            return Err("no translation".to_string());
+        },
+        Err(_) => return Err("post error".to_string())
+    }
+}
+
+// async fn async_translate_into_jananese(phrase: &str) -> Result<String, String> {
+//     Ok("ダミー翻訳".to_string())
+// }
+
+#[tauri::command]
+fn save(file_name: &str, json_str: &str) {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime.block_on(async_save(file_name, json_str))
+}
+
+async fn async_save(file_name: &str, json_str: &str) {
+    let mut file = File::create(file_name);
+    match file {
+        Ok(mut f) => {
+            match f.write_all(json_str.as_bytes()) {
+                Ok(_) => println!("save success"),
+                Err(_) => println!("save error")
+            }
+        },
+        Err(_) => println!("file create error")
+    }
+}
+
+#[tauri::command]
+fn load(file_name: &str) -> Result<String, String> {
+    let runtime = tokio::runtime::Runtime::new().unwrap();
+    runtime.block_on(async_load(file_name))
+}
+
+async fn async_load(file_name: &str) -> Result<String, String> {
+    let content = fs::read_to_string(file_name);
+    match content {
+        Ok(f) => {
+            return Ok(f);
+        },
+        Err(_) => println!("file read error")
+    }
+    
+    Err("unknown error".to_string())
+}
+
+async fn parse_response(response: reqwest::Response) -> Result<TranslationResponse, String> {
+    let res = response.json::<TranslationResponse>().await;
+    match res {
+        Ok(body) => Ok(body),
+        Err(_) => Err("json error".to_string())
+    }
 }
 
 fn main() {
+    dotenv().ok();
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    .invoke_handler(tauri::generate_handler![translate_into_jananese, save, load])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,205 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState, useEffect, useRef } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { Editor, EditorState, Modifier, CompositeDecorator, convertToRaw, convertFromRaw } from "draft-js";
+import "draft-js/dist/Draft.css";
 import "./App.css";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+const TRANSLATE_BLOCK_ENTITY_TYPE = 'TRANSLATE_BLOCK_ENTITY';
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+const SpanEntity = (props: any) => {
+  const phrase = `${props.children[0].props.text}`;
+  const handleMouseEnter = () => {
+    invoke('translate_into_jananese', { phrase: phrase }).then((translatedPhrase: any) => {
+      props.setPhraseDetail({phrase: phrase, translatedPhrase: translatedPhrase});
+    });
+  };
+  return <span className="translate-phrase" onMouseEnter={handleMouseEnter}>{phrase}</span>;
+};
+
+const spanEntityStrategy = (contentBlock: any, callback: any, contentState: any) => {
+  contentBlock.findEntityRanges((character: any) => {
+      const entityKey = character.getEntity();
+      return entityKey !== null && contentState.getEntity(entityKey).getType() === TRANSLATE_BLOCK_ENTITY_TYPE;
+  }, callback);
+};
+
+
+type EditorBlock = {
+  key: string
+  text: string
+  height: number
+  lastTranslatedText: string | null
+}
+
+function App() {
+  const [fileName, setFileName] = useState('');
+  const [editorEnable, setEditorEnable] = useState(false);
+  const [phraseDetail, setPhraseDetail] = useState({phrase: '', translatedPhrase: ''});
+  const [editorState, setEditorState] = useState(() => {
+    const decorators = new CompositeDecorator([
+      {
+          strategy: spanEntityStrategy,
+          component: (decoratorProps: any) => <SpanEntity {...decoratorProps} setPhraseDetail={setPhraseDetail} />
+      }
+    ]);
+    return EditorState.createEmpty(decorators)
+  });
+  const editorRef = useRef<HTMLDivElement>(null);
+  const [blocks, setBlocks] = useState([] as EditorBlock[]);
+  const [translatedMap, setTranslatedMap] = useState({} as { [key: string]: string });
+  const [lastTranslatedMap, setLastTranslatedMap] = useState({} as { [key: string]: string });
+
+  useEffect(() => {
+    setEditorEnable(true);
+  }, []);
+
+  useEffect(() => {
+    let heightMap: { [key: string]: number } = {};
+
+    let elms = editorRef.current?.querySelectorAll('.public-DraftEditor-content > div > div') || [];
+    for (var i = 0; i < elms.length; i++) {
+      let offset_key = elms[i].getAttribute('data-offset-key');
+      if ( offset_key != null) {
+        let key = offset_key.split('-').shift();
+        heightMap[key || ''] = elms[i].getBoundingClientRect().height;
+      }
+    }
+
+    const contentState = editorState.getCurrentContent();
+    const blockArray = contentState.getBlocksAsArray();
+    setBlocks(blockArray.map((block) => {
+      let key = block.getKey();
+      let text = block.getText();
+      console.log(key);
+      console.log(heightMap[key]);
+
+      return {
+        key: key,
+        text: text,
+        height: heightMap[key],
+        lastTranslatedText: lastTranslatedMap[key],
+      }
+    }));
+  }, [editorState])
+
+  const toggleTranslatePhrase = () => {
+    console.log('toggleTranslatePhrase');
+    const content = editorState.getCurrentContent();
+    const selection = editorState.getSelection();
+
+    if (!selection.isCollapsed()) {
+      const startKey = selection.getStartKey();
+      const startOffset = selection.getStartOffset();
+      const blockWithEntityAtStart = content.getBlockForKey(startKey);
+      const entityKeyAtStart = blockWithEntityAtStart.getEntityAt(startOffset);
+
+      // 既にエンティティが適用されているか確認
+      const alreadyHasEntity = entityKeyAtStart && content.getEntity(entityKeyAtStart).getType() === TRANSLATE_BLOCK_ENTITY_TYPE;
+
+      let newContent;
+
+      if (alreadyHasEntity) {
+        // エンティティを取り除く
+        newContent = Modifier.applyEntity(content, selection, null);
+      } else {
+        // エンティティを適用する
+        const contentWithEntity = content.createEntity(TRANSLATE_BLOCK_ENTITY_TYPE, 'MUTABLE');
+        const entityKey = contentWithEntity.getLastCreatedEntityKey();
+        newContent = Modifier.applyEntity(contentWithEntity, selection, entityKey);
+      }
+
+      const newEditorState = EditorState.push(editorState, newContent, 'apply-entity');
+      setEditorState(newEditorState);
+    }
+  };
+
+  const save = () => {
+    const contentState = editorState.getCurrentContent();
+    const content = convertToRaw(contentState);
+    invoke('save', { fileName: fileName, jsonStr: JSON.stringify(content) }).then(() => {
+      console.log('saved');
+    });
+  }
+
+  const load = () => {
+    const contentState = editorState.getCurrentContent();
+    const content = convertToRaw(contentState);
+    const isEmpty = content.blocks.length == 0 || content.blocks.length == 1 && content.blocks[0].text == '';
+    if (!isEmpty) {
+      setPhraseDetail({phrase: 'Load', translatedPhrase: 'コンテンツがあるため、ロードできません'});
+      return;
+    }
+    setPhraseDetail({phrase: 'Load', translatedPhrase: 'ロード中...'});
+
+    invoke('load', { fileName: fileName }).then((jsonStr) => {
+      const contentState = convertFromRaw(JSON.parse(`${jsonStr}`));
+      const newEditorState = EditorState.push(editorState, contentState, 'apply-entity');
+      setEditorState(newEditorState);
+      setPhraseDetail({phrase: 'Load', translatedPhrase: 'ロード完了'});
+    });
   }
 
   return (
     <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="phrase-detail">
+        <div>{phraseDetail.phrase}</div>
+        <div>{phraseDetail.translatedPhrase}</div>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <div className="controll-area">
+      <input className="fileNameField" value={fileName} onChange={(e) => setFileName(e.target.value)} type="text" autoComplete="off" />
+        <div>
+          <button onClick={toggleTranslatePhrase}>Translate</button>
+          <button onClick={save}>Save</button>
+          <button onClick={load}>Load</button>
+        </div>
+      </div>
+      <div className="clearfix"></div>
+      {editorEnable && (
+        <div ref={editorRef} className="all-editor">
+          <Editor editorState={editorState} onChange={setEditorState} />
+          <div className="translate-area-root">
+            <div className="translate-content">
+              <div>
+                {blocks.map((block) => {
+                  return (
+                    <div style={{height: block.height}}>
+                      <span
+                        className="translated-block-button"
+                        onClick={() => {
+                          invoke('translate_into_jananese', { phrase: block.text }).then((translatedPhrase: any) => {
+                            setTranslatedMap((prev) => {
+                              prev[block.key] = translatedPhrase;
+                              return prev;
+                            });
+                            setLastTranslatedMap((prev) => {
+                              prev[block.key] = block.text;
+                              return prev;
+                            });
+                            // 配列を作り直して再描画
+                            setBlocks((prev) => {
+                              var next = [...prev];
+                              for (let i = 0; i < next.length; i++) {
+                                if (next[i].key == block.key) {
+                                  next[i].lastTranslatedText = block.text;
+                                  break;
+                                }
+                              }
+                              return next;
+                            });
+                          });
+                        }}
+                      >
+                        {block.lastTranslatedText == block.text ? '' : '*'}翻訳
+                      </span>
+                      {translatedMap[block.key]}
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
